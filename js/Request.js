e27e086a198949661103a370f28635f16d295e79
Vincent <vl.emile@gmail.com>
2016-01-29 14:55:42 +0100

    [FRONT]Request.js is now JSHint valid

--- js/Request.js
+++ js/Request.js
@@ -1,24 +1,27 @@
+/*global
+Promise,$
+ */
 function Request () {
-
+	"use strict";
 	/**
 	 * Send a request to the server in Synchrone mode
 	 * @file 		{string}
 	 * @parameters 	{string - Object}
-	 * @return 		{Promise}
+	 * @return 		{promise}
 	 */
 	this.send = function (file, parameters) { 
 	 	return new Promise(function(resolve, reject) {
 	 		$.ajax({
 	 			url: file,
 	 			type: 'GET',
-	 			data: parameters,
+	 			data: parameters
 	 		})
 	 		.done(function(data) {
-	 			resolve(data)
+	 			resolve(data);
 	 		})
 	 		.fail(function(data) {
-	 			reject(data)
-	 		})
+	 			reject(data);
+	 		});
 	 	});
 	};
 
