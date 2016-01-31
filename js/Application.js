67fec081d23c6d3fe97a3eca5d21d97d238d9984
armiakrajowa <beaudouink@gmail.com>
2016-02-01 00:20:04 +0100

    [Front][Application] complete launch function in Application

--- js/Application.js
+++ js/Application.js
@@ -1,3 +1,45 @@
 function Application() {
-	
+
+	var url = '';
+	var queryString = '';
+	var type = {};
+	var parser = new Parser();
+
+
+	this.launch = function () {
+		//url = window.location.href;
+		url = 'web/index.html?sensorIdColumn=sensor_id&valuesColumn=value&timestampColumn=timestamp&sensors[measurments]=[70_l,72_c]&startTime=1417962686.2894&endTime=141818181881.2399';
+		queryString = parser.getQueryString(url);
+		console.log('PARAMETERS : ', queryString);
+		
+		type = parser.getType(url);
+		console.log("RESUME : ", type);
+		
+
+        request.send('dbCharts.php', queryString).then(
+            function (result) {
+            	//SUCCESS
+            	var data = JSON.parse(result.responseText);
+            	data = parser.responseForChart(data, type);
+            	var chart = new Chart().construct(parser.getTitle(queryString), data, 'chart');
+            	addEventOnChart(chart);
+            },
+            function (err) {
+            	//FAIL - 4xx - 5xx
+            	var errors = JSON.parse(err);
+            	new ErrorManager.display(errors);
+            }
+        );
+	};
+
+	var addEventOnChart = function(chart){
+		$(window).resize(function(event) {	
+			chart.setSize(
+				$(window).width(), 
+				$(window).height(),
+				false
+			);
+		});
+	};
+
 }
