67d9f6077079268c69d7c2f61fe08e48c3f0f0f2
armiakrajowa <beaudouink@gmail.com>
2016-02-03 09:40:31 +0100

    Correction on ErrorManager

--- js/ErrorManager.js
+++ js/ErrorManager.js
@@ -34,19 +34,24 @@ function ErrorManager() {
      * @return {void}
      */
     this.display = function (errors) {
-        if(errors.errors !== null && errors.errors !== undefined){
+        if(errors !== null && errors !== undefined){
             var res = '';
-                    $(errors.errors).each(function () {
-                        var type;
-                        if(this[error_type] === undefined) {type = 'value';}
-                        else {type = this[error_type];}
-        
-                        res += '<tr><td class="type">'+type+'</td><td class="code">' + this[code] + '</td><td class="key">' + this[key] + '</td><td class="message">' + this[message] + '</td></tr>';
-                    });
-        
-                    var table = '<table class="table" id="error-table"><thead><th>Type</th><th>Code</th><th>Key</th><th>Message</th></thead>' + res + '</table>';
-        
-                    this.createModal(table);
+            $(errors).each(function () {
+                var type;
+                var key;
+                if(this[error_type] === undefined) {type = 'value';}
+                else {type = this[error_type];}
+
+                if(this[key] === undefined) {key = '';}
+                else {key = this[key];}
+
+
+                res += '<tr><td class="type">'+type+'</td><td class="code">' + this[code] + '</td><td class="key">' + key + '</td><td class="message">' + this[message] + '</td></tr>';
+            });
+
+            var table = '<table class="table" id="error-table"><thead><th>Type</th><th>Code</th><th>Key</th><th>Message</th></thead>' + res + '</table>';
+
+            this.createModal(table);
         }
     };
 
